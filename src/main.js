7626fa06ca76f3d23b6c6ee55fc9e1d5017eed10
Jessica Birchfield <birchj4@cs.washington.edu>
2020-02-26 16:25:12 -0800

    testing svgs

--- src/main.js
+++ src/main.js
@@ -13,6 +13,22 @@ const d3 = require('d3');
  const bigChartInstance = new BigChart();
  const smallChartInstance = new SmallChart();
 
+ smallChartInstance.sayHi();
+
+// create svg for smallChart
+ const smallsvg = d3.select('#smallchart')
+             .append('svg')
+             .attr('width', 1200)
+             .attr('height',420);
+smallsvg.append("circle")
+       .attr("cx", 15)
+       .attr("cy", 15)
+       .attr("r", 10)
+       .style("fill", "black");
+console.log(smallsvg);
+console.log("d3 select: ", d3.select('#smallchart'));
+smallChartInstance.drawRank(smallsvg);
+
 // You can load JSON files directly via require.
 // Note this does not add a network request, it adds
 // the data directly to your JavaScript bundle.
--- src/smallChart.js
+++ src/smallChart.js
@@ -1,15 +1,29 @@
-
-
 // You can separate your code out into modules to
 // keep code clean.
+const d3 = require('d3');
 
 class smallChart {
+  //static smallsvg;
+
   constructor() {
+    // smallsvg = d3.select('#smallchart')
+    //       .append('svg')
+    //       .attr('width',420)
+    //       .attr('height',400);
 
   }
 
   sayHi() {
     console.log('[MyClass]', 'Hello World.');
+  }
+
+  drawRank(smallsvg) {
+    console.log('draw rank');
+
+    smallsvg.append("text")
+        .attr("x", 70)
+        .attr("y", 200)
+        .text("Look it's on the page");
   }
 }
 
