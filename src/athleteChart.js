32dd3107745c50dc97adbc57b608da15abb366a7
Millicent Li <limill01@cs.washington.edu>
2020-03-10 15:53:51 -0700

    added the backbone for the athlete chart

---
+++ src/athleteChart.js
@@ -0,0 +1,13 @@
+const d3 = require('d3');
+
+class athleteChart {
+
+  constructor(data, columns) {
+    this.data = data;
+    this.columns = columns;
+
+  }
+
+};
+
+module.exports = athleteChart;
