aa27e5ee6e3a31531694a91c7a311bb3a4a7c484
Bin <binphan@Bin-Laptop.localdomain>
2020-03-10 14:06:31 -0700

    Selected line will stay highlighted

--- src/bigChart.js
+++ src/bigChart.js
@@ -17,7 +17,7 @@ class bigChart {
     this.margin = 50;
     // this.margins = { top: 30, right: 10, bottom: 10, left: 10 };
     this.margins = { top: 30, right: 35, bottom: 10, left: 35 };
-  
+
     // getting scale of graph
 
     // adding tiny chart
@@ -292,53 +292,54 @@ class bigChart {
       .style('fill', 'none')
       .on("mouseover", function (d) {
         if (selectedCountry === undefined) {
-        // change line opacity
-        d3.selectAll(".line")
-          .style('opacity', otherLinesOpacityHover)
-        d3.select(this)
-          .style('opacity', lineOpacityHover)
-          .style('stroke-width', lineStrokeHover);
-        // add text to show what country this is
-        svg.append("text")
-          .text(d.key)
-          .attr('class', 'country-text')
-          .attr("x", (width - margin) / 2)
-          .attr("y", 15)
-          .style('fill', color(d.key))
-      }
-      else if (selectedCountry === d) {
-        d3.select(this)
-        .style('stroke', 'black')
-      }
-    })
+          // change line opacity
+          d3.selectAll(".line")
+            .style('opacity', otherLinesOpacityHover)
+          d3.select(this)
+            .style('opacity', lineOpacityHover)
+            .style('stroke-width', lineStrokeHover);
+          // add text to show what country this is
+          svg.append("text")
+            .text(d.key)
+            .attr('class', 'country-text')
+            .attr("x", (width - margin) / 2)
+            .attr("y", 15)
+            .style('fill', color(d.key))
+        }
+        else if (selectedCountry === d) {
+          d3.select(this)
+            .style('stroke', 'black')
+        }
+      })
       .on("mouseout", function (d) {
         if (selectedCountry === undefined) {
-        d3.selectAll(".line")
-          .style("opacity", lineOpacity)
-          .style("stroke-width", lineStroke)
-        // d3.select(this)
-        //   .style('opacity', lineOpacity)
-        //   .style("stroke-width", lineStroke);
-        svg.selectAll(".country-text").remove();
-      }
-      else if (selectedCountry === d) {
-        d3.select(this)
-          .style('stroke', d => color(d.key))
-      }
-    })
+          d3.selectAll(".line")
+            .style("opacity", lineOpacity)
+            .style("stroke-width", lineStroke)
+          // d3.select(this)
+          //   .style('opacity', lineOpacity)
+          //   .style("stroke-width", lineStroke);
+          svg.selectAll(".country-text").remove();
+        }
+        else if (selectedCountry === d) {
+          d3.select(this)
+            .style('stroke', d => color(d.key))
+        }
+      })
       .on("click", function (d) {
         // get the data for the selected athlete
         selectedCountry = selectedCountry === undefined ? d : undefined;
         if (selectedCountry === d) {
           d3.selectAll(".line")
-          .style('opacity', otherLinesOpacitySelected)
-        d3.select(this)
-          .style('opacity', lineOpacityHover)
-          .style('stroke-width', lineStrokeHover);
+            .style('opacity', otherLinesOpacitySelected)
+          d3.select(this)
+            .style('opacity', lineOpacityHover)
+            .style('stroke-width', lineStrokeHover);
         } else {
           d3.selectAll(".line")
-          .style("opacity", lineOpacity)
-          .style("stroke-width", lineStroke)
+            .style("opacity", lineOpacity)
+            .style("stroke-width", lineStroke)
+          return;
         }
         console.log(entriesBySportThenCountryThenYear)
         console.log(d);
@@ -398,11 +399,11 @@ class bigChart {
         // console.log(d);
         // xScale(d), 0], [xScale(d) + 5, this.height
         // d3.brushY().extent([0, 0], [100, 200])
-        d3.select(this).call(brushRange[d] = d3.brushY().extent([[xScale(d) - 8, 0], [xScale(d) + 8, yScale(0)]]).on("brush", function() {
+        d3.select(this).call(brushRange[d] = d3.brushY().extent([[xScale(d) - 8, 0], [xScale(d) + 8, yScale(0)]]).on("brush", function () {
           console.log("yo");
         }).on("brush", brush)) //TODO: change 600 to be this.height
       })
-    this.brushRange = brushRange;  
+    this.brushRange = brushRange;
   }
 
 
