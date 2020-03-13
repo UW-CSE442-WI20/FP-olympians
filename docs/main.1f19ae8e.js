15fa973018ff2e5fb59fc045b3ed561cc69b3261
1MmM1 <lin-family@live.com>
2020-03-13 01:36:57 -0700

    Build with Myanmar

--- docs/main.1f19ae8e.js
+++ docs/main.1f19ae8e.js
@@ -43861,7 +43861,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52859" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52974" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
